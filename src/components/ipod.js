1d8b6c0042c31644ed135c2d407617c4201cc9fd
Sagar Kaushik <sagar302001@gmail.com>
2024-10-13 19:37:56 +0530

    Added pointer Event

--- src/components/ipod.js
+++ src/components/ipod.js
@@ -1,62 +1,84 @@
 import { Component } from "react";
-import IpodStyle from "./ipod.module.css"
+import IpodStyle from "./ipod.module.css";
 
-export default class Ipod extends Component{
-
-    constructor(){
+export default class Ipod extends Component {
+    constructor() {
         super();
-        this.state ={
-            activeItem: 0
+        this.state = {
+            activeItem: 0,
+        };
+        this.isPointerActive = false; // Track if pointer is down
+    }
+
+    handleItemClick = (index) => {
+        this.setState({ activeItem: index }, () => {
+            console.log(this.state.activeItem);
+        });
+    };
+
+    handlePointerMove = () => {
+        if (this.isPointerActive) {
+            this.setState((prevState) => {
+                const nextActiveItem = (prevState.activeItem + 1) % 4; // Cycle through items
+                return { activeItem: nextActiveItem };
+            });
         }
+    };
+
+    handlePointerDown = () => {
+        this.isPointerActive = true; // Set pointer active
+        this.pointerMoveInterval = setInterval(this.handlePointerMove, 500); // Call move function every 100ms
+    };
+
+    handlePointerUp = () => {
+        this.isPointerActive = false; // Set pointer inactive
+        clearInterval(this.pointerMoveInterval); // Clear interval
+    };
+
+    componentWillUnmount() {
+        clearInterval(this.pointerMoveInterval); // Clean up on unmount
     }
-    handleItemClick =(index)=>{
-        this.setState({ activeItem: index },()=>{console.log(this.state.activeItem)});
-    }
-    handlePointerMove = (e)=>{
-        console.log("Pointer is moving:", e.clientX, e.clientY);
-        if(this.state.activeItem < 4){
-            this.setState({ activeItem: this.state.activeItem+1 }) 
-        }else{
-            this.setState({ activeItem: 0 }) 
-        }
-        
-    }
-    render(){
+
+    render() {
         const items = ['CoverFlow', 'Music', 'Games', 'Setting'];
-        return(
+        return (
             <div className={IpodStyle.mainDivIpod}>
-                {/* Screen Section */}
                 <div className={IpodStyle.IpodScreen}>
-                 <section className={IpodStyle.screenMainSection}>
-                    <p>Ipod</p>
-                    <ul>
-                        {items && items.map((item,index)=>(
-                            <li
-                            key={index}
-                            className={this.state.activeItem === index ? IpodStyle.active : ''}
-                            onClick={() => this.handleItemClick(index)}
-                    >{item}</li>
-                        ))}
-                    </ul>
-                 </section>
+                    <section className={IpodStyle.screenMainSection}>
+                        <p>Ipod</p>
+                        <ul>
+                            {items.map((item, index) => (
+                                <li
+                                    key={index}
+                                    className={this.state.activeItem === index ? IpodStyle.active : ''}
+                                    onClick={() => this.handleItemClick(index)}
+                                >
+                                    {item}
+                                </li>
+                            ))}
+                        </ul>
+                    </section>
                 </div>
-                {/* Button Section */}
                 <div className={IpodStyle.IpodButtonSection}>
-                 <div className={IpodStyle.buttonsTopMainSection} onPointerMove={(e)=>this.handlePointerMove(e)}>
-                    <section className={IpodStyle.menuButtonSection}>
-                        <p></p>
-                    </section>
-                    <section className={IpodStyle.nextbuttonSection}>
-                    <img className={IpodStyle.prevButton} src="https://cdn-icons-png.flaticon.com/128/2723/2723003.png" alt="icon"></img>
-                    <section className={IpodStyle.centerButton}></section>
-                    <img className={IpodStyle.nextButton} src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="icon"></img>
-                    </section>
-                    <section className={IpodStyle.pauseButtonSection}>
-                    <img  className={IpodStyle.pauseButton}src="https://cdn-icons-png.flaticon.com/128/2066/2066181.png" alt="icon"></img>
-                    </section>
-                 </div>
+                    <div
+                        className={IpodStyle.buttonsTopMainSection}
+                        onPointerDown={this.handlePointerDown}
+                        onPointerUp={this.handlePointerUp}
+                    >
+                        <section className={IpodStyle.menuButtonSection}>
+                            <p></p>
+                        </section>
+                        <section className={IpodStyle.nextbuttonSection}>
+                            <img className={IpodStyle.prevButton} src="https://cdn-icons-png.flaticon.com/128/2723/2723003.png" alt="icon" />
+                            <section className={IpodStyle.centerButton}></section>
+                            <img className={IpodStyle.nextButton} src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="icon" />
+                        </section>
+                        <section className={IpodStyle.pauseButtonSection}>
+                            <img className={IpodStyle.pauseButton} src="https://cdn-icons-png.flaticon.com/128/2066/2066181.png" alt="icon" />
+                        </section>
+                    </div>
                 </div>
             </div>
-        )
+        );
     }
 }
